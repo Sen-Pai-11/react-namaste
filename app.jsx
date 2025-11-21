50e5fd15e2d8cf2d21aff0c814745bcce581c5dc
Yash Panchal <yashpanchal5320@gmail.com>
2025-11-21 20:52:00 +0530

    first commit

---
+++ app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppLayout from "./components/AppLayout";
+
+
+const heading = () => {
+    return <h1>Hello</h1>
+}
+    
+export default heading;
