d1b04b2d0e5576521219f2808f5da8306e2d41af
Yash Panchal <yashpanchal5320@gmail.com>
2025-12-11 20:11:27 +0530

    added hooks

---
+++ src/utils/constants.jsx
@@ -0,0 +1,7 @@
+
+
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/29/62cada56-2bbe-44a1-9172-d07af4c77d33_28721.jpg";
+
+export const LOGO_URL =
+  "https://imgs.search.brave.com/MSqooKbITwtQfFNAUPbEI6V1hD2_1GB-rW7uT7aVKTc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvZ29vZC1mb29k/LWxvZ28tZGVzaWdu/Xzc5MTY5LTEwLmpw/Zz9zZW10PWFpc19o/eWJyaWQmdz03NDAm/cT04MA";
